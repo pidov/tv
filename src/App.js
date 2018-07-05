6e683f9317e658554ee9fddcc0c737284d3fe6bd
Ivan Pidov <ivan.pidov@tick42.com>
2018-07-05 18:41:31 +0300

    Basic POC

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Slider from "react-slick"
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      slides: [
+        'HBO GO',
+        'YouTube',
+        'Plex',
+        'Twitch',
+        'Spotify',
+        'TV',
+        'Games',
+        'Browser'
+      ]
+    }
+  }
+
+  componentDidMount () {
+    ReactDOM.findDOMNode(this.refs.slider).focus()
+    console.log(ReactDOM.findDOMNode(this.refs.slider))
+  }
+
+  render () {
+    const settings = {
+      arrows: false,
+      infinite: true,
+      className: 'center',
+      centerMode: true,
+      centerPadding: '20px',
+      speed: 150,
+      slidesToShow: 5,
+      slidesToScroll: 1
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2 tabIndex={123} onKeyDown={e => {console.log(e)}}> Single Item</h2>
+        <Slider tabIndex={231} {...settings} ref='slider'>
+          {this.state.slides.map(slide => (
+            <div key={slide} className='slide'>
+              <h3>{slide}</h3>
+            </div>
+          ))}
+        </Slider>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
